0324c52e35aa07842d407b76ad83681ee605d5a8
JarnKet <ketsadaphoneOfficial@gmail.com>
2025-09-29 15:29:48 +0700

    feat: add CI Pipeline

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting Testing the application...");
+setTimeout(function () {
+    console.log("Waiting for 3 seconds to complete the testing...")
+}, 3000);
+console.log("Testing Completed Successfully!");
